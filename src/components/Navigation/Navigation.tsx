9076a18aea18d85da06496d02de12de9a8c0b13b
BELLORI <bellori729@gmail.com>
2024-02-26 17:44:46 +0900

    [기타] (kim/#9): 테일윈드 클래스 스타일 자동정렬

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,7 +2,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const Navigation = () => {
   return (
-    <nav className="rounded-tl-14px rounded-tr-14px flex h-80px w-375px items-center justify-center gap-25px bg-white shadow-md">
+    <nav className="flex h-80px w-375px items-center justify-center gap-25px rounded-tl-14px rounded-tr-14px bg-white shadow-md">
       <NavigationItem isHomeActive={false}></NavigationItem>
       <NavigationItem isBoxActive={false}>습득물</NavigationItem>
       <NavigationItem isLostActive={false}>분실물</NavigationItem>
