322f049f974b2997c0f0cd1bcf4a507723941a47
zooyaam <yjoo2498@naver.com>
2024-02-27 17:45:25 +0900

    [코드 수정] (cho/#17) 로고 이미지 위치 수정

--- src/components/pages/Splash.tsx
+++ src/components/pages/Splash.tsx
@@ -3,7 +3,7 @@ import logo from '@/assets/logo.svg';
 const Splash = () => {
   return (
     <div className="flex h-667px w-375px items-center justify-center bg-gradient-to-br from-white to-blue-100">
-      <img src={logo} alt="찾아줘 메인 로고" />
+      <img src={logo} alt="찾아줘 메인 로고" className="pb-5" />
     </div>
   );
 };
