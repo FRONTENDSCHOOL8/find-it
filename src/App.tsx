b9cc6922bbc9d67fd5700fdbd2697b10cd705f53
1101x <junggg14@gmail.com>
2024-02-27 10:28:57 +0900

    [기타](jung/#8) 충동 해결2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useEffect } from 'react';
+import useCountStore from '@/store/count/countStore';
 import SignIn from '@/components/SignIn/SignIn';
 
 const App = () => {
-  return <SignIn />;
+  const { count } = useCountStore((state) => state);
+  console.log(count);
+
+  useEffect(() => {
+    const openApiParams = new URLSearchParams({
+      key: import.meta.env.VITE_PUBLICINFO_API_KEY_DEC,
+    });
+
+    const data = fetch(
+      `${import.meta.env.VITE_PUBLICINFO_API_URL}?${openApiParams}`
+    )
+      .then((res) => res.text())
+      .catch((err) => console.error(err));
+
+    console.log(data);
+  }, []);
+
+  return (
+    <div className="text-center">
+      <h1 className="text-32px text-red-500">Find It</h1>
+      <SignIn />;
+    </div>
+  );
 };
 
 export default App;
