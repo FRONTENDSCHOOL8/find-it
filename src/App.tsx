fce3632f89989dc2e11467a849c7d02c5e184bea
1101x <junggg14@gmail.com>
2024-02-27 10:07:37 +0900

    [기능](jung/#8) 로그인 마크업 기본 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
-import { useEffect } from 'react';
-import useCountStore from '@/store/count/countStore';
+import SignIn from '@/components/SignIn/SignIn';
 
 const App = () => {
-  const { count } = useCountStore((state) => state);
-  console.log(count);
-
-  useEffect(() => {
-    const openApiParams = new URLSearchParams({
-      key: import.meta.env.VITE_PUBLICINFO_API_KEY_DEC,
-    });
-
-    const data = fetch(
-      `${import.meta.env.VITE_PUBLICINFO_API_URL}?${openApiParams}`
-    )
-      .then((res) => res.text())
-      .catch((err) => console.error(err));
-
-    console.log(data);
-  }, []);
-
-  return (
-    <div className="text-center">
-      <h1 className="text-32px text-red-500">Find It</h1>
-    </div>
-  );
+  return <SignIn />;
 };
 
 export default App;
