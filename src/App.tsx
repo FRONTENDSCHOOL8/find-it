18116189650eb62c7c919c5e8abb4b7f98f0c24c
1101x <junggg14@gmail.com>
2024-02-29 00:41:03 +0900

    [기타](jung/#37)테스트용 App.tsx 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Detail from './components/Detail/Detail';
-import SignIn from './components/SignIn/SignIn';
 
 const App = () => {
   return (
     <div className="w-mobile">
       <h1>App</h1>
-      {/* <Detail>sdf</Detail> */}
-      <SignIn />
+      <Detail>sdf</Detail>
     </div>
   );
 };
