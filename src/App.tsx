b41bd0c64e09b1049528c60a2335855090dfef55
zooyaam <yjoo2498@naver.com>
2024-02-28 10:17:57 +0900

    [기능] React Router 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
-import { useEffect } from 'react';
-import useCountStore from '@/store/count/countStore';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  const { count } = useCountStore((state) => state);
-  console.log(count);
-
-  useEffect(() => {
-    const openApiParams = new URLSearchParams({
-      key: import.meta.env.VITE_PUBLICINFO_API_KEY_DEC,
-    });
-
-    const data = fetch(
-      `${import.meta.env.VITE_PUBLICINFO_API_URL}?${openApiParams}`
-    )
-      .then((res) => res.text())
-      .catch((err) => console.error(err));
-
-    console.log(data);
-  }, []);
-
   return (
-    <div className="text-center">
-      <h1 className="text-32px text-red-500">Find It</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
