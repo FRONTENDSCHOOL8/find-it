3c2f1f6ce78ba70cb6eca91d6ac1ba48dd6646f8
ksyee <tjssud0647@gmail.com>
2024-03-04 13:41:11 +0900

    [기능] (kang#72) Router로 page연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Detail from './components/Detail/Detail';
+import {
+  Main,
+  MyPage,
+  Splash,
+  SignIn,
+  SignUp,
+  Credit,
+} from '@/components/pages/index';
+import Detail from '@/components/Detail/Detail';
 
 const App = () => {
   return (
     <>
-      <Detail>asdf</Detail>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/splash" element={<Splash />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/credit" element={<Credit />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/components/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Main } from '@/components/pages/Main';
+export { default as Splash } from '@/components/pages/Splash';
+export { default as MyPage } from '@/components/pages/MyPage';
+export { default as SignIn } from '@/components/pages/SignIn';
+export { default as SignUp } from '@/components/pages/SignUp';
+export { default as Credit } from '@/components/pages/Credit';
