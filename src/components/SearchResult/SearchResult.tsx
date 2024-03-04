509d37835b058c863e048efc9c593624e7a36b47
BELLORI <bellori729@gmail.com>
2024-03-04 15:20:13 +0900

    [코드 수정] (kim/#74): gap 삭제

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -3,7 +3,7 @@ import ItemBox from '../ItemBox/ItemBox';
 
 const SearchResult = () => {
   return (
-    <div className="flex h-667px min-w-375px max-w-400px flex-col items-center gap-18px overflow-y-auto bg-gray-200">
+    <div className="flex h-667px min-w-375px max-w-400px flex-col items-center overflow-y-auto bg-gray-200">
       <Header isShowPrev={true} empty={true}>
         검색결과
       </Header>
