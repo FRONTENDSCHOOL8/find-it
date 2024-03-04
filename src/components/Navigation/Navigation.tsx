692a026dc2a409d3c45ea773e2761b3d0a1e5550
BELLORI <bellori729@gmail.com>
2024-03-04 15:36:26 +0900

    [코드 수정] (kim/#74): Navigation 컴포넌트 하단 고정

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,7 +2,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const Navigation = () => {
   return (
-    <nav className="flex h-80px w-375px items-center justify-center gap-25px rounded-tl-14px rounded-tr-14px bg-white shadow-md">
+    <nav className="fixed bottom-0 left-1/2 flex h-80px w-375px -translate-x-1/2 transform items-center justify-center gap-25px rounded-tl-14px rounded-tr-14px bg-white shadow-md">
       <NavigationItem isHomeActive={false}></NavigationItem>
       <NavigationItem isBoxActive={false}>습득물</NavigationItem>
       <NavigationItem isLostActive={false}>분실물</NavigationItem>
