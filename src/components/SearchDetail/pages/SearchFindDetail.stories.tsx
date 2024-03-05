a1ba8c9d22b84745d52d5ab396f03640801f5618
BELLORI <bellori729@gmail.com>
2024-03-05 09:49:17 +0900

    [코드 수정] (kim/#80): - SearchDetail -> SearchFindDetail, SearchLostDetail로 분할 - 이동하기 링크 연결

---
+++ src/components/SearchDetail/pages/SearchFindDetail.stories.tsx
@@ -0,0 +1,10 @@
+import { StoryObj } from '@storybook/react';
+import SearchFindDetail from './SearchFindDetail';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: SearchFindDetail,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const Basic: StoryObj<typeof SearchFindDetail> = {};
