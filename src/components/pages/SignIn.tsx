fb8a09442533873635dc383b3c0cef786632e4f3
1101x <junggg14@gmail.com>
2024-03-06 15:47:14 +0900

    [기능](jung/#89) 로그인 페이지 기능 완료

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
+import { pb } from '@/lib/api/getPbData';
+import { getData } from '@/lib/utils/crud';
 import Header from '@/components/Header/Header';
-import Navigation from '@/components/Navigation/Navigation';
 import InputForm from '@/components/SignIn/molecule/InputForm';
+import Navigation from '@/components/Navigation/Navigation';
 import ButtonVariable from '@/components/common/molecule/ButtonVariable';
-
+/* -------------------------------------------------------------------------- */
+// 머지 테스트
 //알럿창 타입 정의
 type AlertProps =
   | 'doubleCheckEmail'
@@ -27,8 +30,9 @@ const SignIn = () => {
 
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
+
   /* -------------------------------------------------------------------------- */
-  //함수
+  // 입력 함수
   const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const newValue = e.target.value;
@@ -54,12 +58,13 @@ const SignIn = () => {
     setAlertPassword('');
   };
   /* -------------------------------------------------------------------------- */
-  // 회원가입 가기
-  const handleSignup = (e: React.FormEvent<HTMLFormElement>) => {
+  // 회원가입 버튼
+  const handleSignUp = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     window.location.href = '/signup';
   };
-  // 로그인 버튼 조건
+
+  // 로그인 버튼 활성화 조건 : 빈문자열만 아니면 됨
   const [variant, setVariant] = useState<'submit' | 'disabled'>('disabled');
   useEffect(() => {
     if (emailValue !== '' && passwordValue !== '') {
@@ -70,9 +75,36 @@ const SignIn = () => {
   }, [emailValue, passwordValue]);
 
   // 최종 로그인 버튼
-  const handleSignIn = () => {
-    alert('로그인 버튼 클릭 테스트');
+  const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    (async () => {
+      // 이메일 있는지 확인
+      const records = await getData('users', {
+        filter: `email="${emailValue}"`, //pb 에서 조건 충족 리스트 가져옴(객체 1개배열)
+      });
+      const realdata = records && records[0];
+      const emailData = realdata && realdata.email;
+      if (emailData === emailValue) {
+        /// 이메일 같을 경우 권한 db 전송
+        try {
+          const authData = await pb
+            .collection('users')
+            .authWithPassword(emailValue, passwordValue);
+          alert(`${authData.record?.nickname}님 좋은 하루 보내세요!`);
+          window.location.href = '/';
+        } catch (error) {
+          alert('비밀번호를 확인해주세요.');
+        }
+      } else {
+        alert('회원정보가 없습니다. 회원가입을 해주세요.');
+      }
+    })();
   };
+  // 로그아웃 기능
+  // const handleSignOut = () => {
+  //   pb.authStore.clear();
+  // };
+  /* -------------------------------------------------------------------------- */
   /* -------------------------------------------------------------------------- */
   // 마크업
   return (
@@ -112,7 +144,7 @@ const SignIn = () => {
               <ButtonVariable
                 buttonText="회원가입"
                 variant="lineStyle"
-                onClick={handleSignup}
+                onClick={handleSignIn}
               />
             </div>
           </form>
