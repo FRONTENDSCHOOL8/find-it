2cfaa2116a18ee853716cdf4ef6bdf9f7abfab83
zooyaam <yjoo2498@naver.com>
2024-03-10 02:35:19 +0900

    [기능] (cho/#126) 메인 물품 박스 렌더링

--- src/components/ItemBox/ItemBox.tsx
+++ src/components/ItemBox/ItemBox.tsx
@@ -1,5 +1,5 @@
-import default_item from '@/assets/itembox/default_item.svg';
 import { useEffect, useState } from 'react';
+import default_item from '@/assets/itembox/default_item.svg';
 
 type itemTypeProps = {
   itemType: 'get' | 'lost' | 'main';
@@ -129,29 +129,40 @@ const ItemBox: React.FC<itemTypeProps> = ({ itemType, item }) => {
         </a>
       )}
 
-      {itemType === 'main' && (
+      {itemType === 'main' && getItemData && (
         <a href="/" className="block">
           <div className="mb-3 flex h-140px w-335px justify-between rounded-[20px] bg-primary transition-all duration-300 hover:cursor-pointer hover:shadow-lg">
             <div className="flex flex-col items-start py-18px pl-20px">
               <h1 className="pb-2 text-20px font-medium leading-[1.3] tracking-tighter text-white">
-                물품명
+                {getItemData.get_item_name.length > 10
+                  ? getItemData.get_item_name.slice(0, 8) + '...'
+                  : getItemData.get_item_name}
               </h1>
               <span className="rounded-full bg-white px-3 py-1 text-10px font-medium leading-[1.3] tracking-tighter text-primary ">
-                습득장소
+                {getItemData.storage}
               </span>
 
               <div className="mt-13px flex flex-col gap-1">
                 <span className="text-12px font-medium leading-[1.3] tracking-tighter text-skyblue-400">
-                  습득한 날
+                  습득날짜
                 </span>
                 <span className="text-12px font-medium leading-[1.3] tracking-tighter text-white">
-                  2024년 2월 26일
+                  {getItemData.get_date}
                 </span>
               </div>
             </div>
 
             <div className="p-10px">
-              <img src={default_item} alt="등록된 물품 사진 없음" />
+              <img
+                src={
+                  getItemData.item_image ===
+                  'https://www.lost112.go.kr/lostnfs/images/sub/img02_no_img.gif'
+                    ? default_item
+                    : getItemData.item_image
+                }
+                alt="물품 사진"
+                className="size-120px rounded-14px"
+              />
             </div>
           </div>
         </a>
