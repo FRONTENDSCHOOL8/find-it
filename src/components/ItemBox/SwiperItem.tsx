131a4ebb6f1e0811a2e5fcf2953da576746a033f
BELLORI <bellori729@gmail.com>
2024-03-12 18:12:07 +0900

    [기능] (kim/#164): SwiperItem 스켈레톤 UI 적용

--- src/components/ItemBox/SwiperItem.tsx
+++ src/components/ItemBox/SwiperItem.tsx
@@ -8,20 +8,29 @@ import { getAllData } from '@/lib/utils/getAPIData';
 import { Autoplay, Pagination, Keyboard } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 import '@/tailwind.css';
+import Skeleton from './Skeleton';
 
 SwiperCore.use([Autoplay, Pagination, Keyboard]);
 
 const SwiperItem: React.FC = () => {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     (async () => {
+      setIsLoading(true);
       const data = await getAllData({
         pageNo: 1,
         numOfRows: 3,
       });
       setItems(data as JsonArray);
+      setIsLoading(false);
     })();
   }, []);
+
+  if (isLoading) {
+    return <Skeleton />;
+  }
 
   return (
     <Swiper
