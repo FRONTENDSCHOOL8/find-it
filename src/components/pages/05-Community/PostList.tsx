e669258920ffe8ad40e43671d11c2a770edf3179
BELLORI <bellori729@gmail.com>
2024-03-13 15:48:40 +0900

    [코드 수정] (kim/#180): 자유게시판 레이아웃 수정

--- src/components/pages/05-Community/PostList.tsx
+++ src/components/pages/05-Community/PostList.tsx
@@ -9,30 +9,30 @@ const PostList = () => {
   const loginUserData = localStorage.getItem('pocketbase_auth');
 
   return (
-    <>
-      <div className="flex w-full flex-col items-center">
-        <Header
-          isShowSymbol={true}
-          children="자유게시판"
-          isShowSearch={true}
-          link="/searchpost"
-        />
-        <Horizon lineBold="thin" lineWidth="long" />
-        <div className="h-[calc(100vh-66px-80px)] w-full overflow-auto">
+    <div className="flex h-screen w-full flex-col items-center">
+      <Header
+        isShowSymbol={true}
+        children="자유게시판"
+        isShowSearch={true}
+        link="/searchpost"
+      />
+      <Horizon lineBold="bold" lineWidth="long" />
+      <div className="relative w-375px">
+        <div className="h-[calc(100vh-66px-80px)] overflow-auto">
           <PostBox />
         </div>
+        {loginUserData && (
+          <Link to="/createpost">
+            <img
+              src={IconPlus}
+              alt="글쓰기 버튼"
+              className="absolute bottom-18px right-30px z-10 size-60px drop-shadow-xl hover:animate-bounce	 "
+            />
+          </Link>
+        )}
       </div>
-      {loginUserData && (
-        <Link to="/createpost">
-          <img
-            src={IconPlus}
-            alt="글쓰기 버튼"
-            className="fixed bottom-100px right-24px z-10 size-60px drop-shadow-xl hover:animate-bounce	 "
-          />
-        </Link>
-      )}
       <Navigation />
-    </>
+    </div>
   );
 };
 
