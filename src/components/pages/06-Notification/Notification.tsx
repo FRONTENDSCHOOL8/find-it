deb0913344dfd5ecaf86a03bdc2596b971263bf2
zooyaam <yjoo2498@naver.com>
2024-03-13 17:21:31 +0900

    [기능] (cho/#122) 알림 아이콘 표시

--- src/components/pages/06-Notification/Notification.tsx
+++ src/components/pages/06-Notification/Notification.tsx
@@ -1,22 +1,41 @@
+import { useState, useEffect } from 'react';
 import Header from '@/components/Header/Header';
 import Notice from '@/components/pages/06-Notification/Notice';
 import Setting from '@/components/pages/06-Notification/Setting';
-import { useState } from 'react';
 
 const Categories = ({ onChangeCategory }) => {
+  const [voidAlarmIcon, setVoidAlarmIcon] = useState(false);
   const [activeButton, setActiveButton] = useState('notice');
   const handleClick = (button) => {
     setActiveButton(button);
     onChangeCategory(button);
   };
 
+  // 추천 알림이 있으면 스타일 표시
+  useEffect(() => {
+    const savedRecommendations = localStorage.getItem('recommendations');
+
+    if (savedRecommendations === '[]') {
+      setVoidAlarmIcon(false);
+    } else {
+      setVoidAlarmIcon(true);
+    }
+  }, []);
+
   return (
     <div className="flex w-375px">
       <button
-        className={`w-1/2 border-b-[1px] pb-12px pt-13px text-center text-14px transition-colors duration-300 ${activeButton === 'notice' ? 'border-black' : 'text-gray-400'}`}
+        className={`relative w-1/2 border-b-[1px] pb-12px pt-13px text-center text-14px transition-colors duration-300 ${activeButton === 'notice' ? 'border-black' : 'text-gray-400'}`}
         onClick={() => handleClick('notice')}
       >
         내 키워드 알림
+        {voidAlarmIcon && (
+          <p
+            className={`absolute right-37px top-13px h-7px w-7px rounded-full bg-primary ${activeButton === 'setting' ? 'opacity-70' : ''}`}
+          >
+            &nbsp;
+          </p>
+        )}
       </button>
       <button
         className={`w-1/2 border-b-[1px] pb-12px pt-13px text-center text-14px transition-colors duration-300 ${activeButton === 'setting' ? 'border-black' : 'text-gray-400'}`}
