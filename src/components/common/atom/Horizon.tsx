0aa0b59abb1a328795bc5960ad0f96283e7dd0a4
BELLORI <bellori729@gmail.com>
2024-03-13 15:30:59 +0900

    [코드 수정] (kim/#180): 긴 줄 width값 조정

--- src/components/common/atom/Horizon.tsx
+++ src/components/common/atom/Horizon.tsx
@@ -13,9 +13,9 @@ const Horizon: React.FC<HorizonProps> = ({ lineBold, lineWidth }) => {
   } else if (lineBold === 'bold' && lineWidth === 'short') {
     style = 'h-1px w-315px bg-gray-300';
   } else if (lineBold === 'thin' && lineWidth === 'long') {
-    style = 'h-[0.4px] w-full bg-gray-300';
+    style = 'h-[0.4px] w-375px bg-gray-300';
   } else if (lineBold === 'bold' && lineWidth === 'long') {
-    style = 'h-1px w-full bg-gray-300';
+    style = 'h-1px w-375px bg-gray-300';
   }
 
   return <div className={style}></div>;
