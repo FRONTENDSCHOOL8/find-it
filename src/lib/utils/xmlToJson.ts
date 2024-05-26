980f93689f8774d2bb36fd42e88613d649f4e6a6
ksyee <tjssud0647@gmail.com>
2024-05-26 17:52:44 +0900

    [refactor] xmlToJson.ts 성능 최적화 - Array.from(xml.attributes).reduce 대신 단순 반복문 사용 - Array.from(xml.childNodes) 대신 xml.childNodes를 직접 순회하여 재귀 호출

--- src/lib/utils/xmlToJson.ts
+++ src/lib/utils/xmlToJson.ts
@@ -7,13 +7,21 @@ export const xmlToJson = (xml: Node): JsonObject | string => {
   if (xml.nodeType === 1 && xml instanceof Element) {
     // 요소 노드
     if (xml.hasAttributes()) {
-      obj['@attributes'] = Array.from(xml.attributes).reduce<JsonObject>(
-        (acc, attr) => {
-          acc[attr.nodeName] = attr.nodeValue || '';
-          return acc;
-        },
-        {}
-      );
+      // obj['@attributes'] = Array.from(xml.attributes).reduce<JsonObject>(
+      //   (acc, attr) => {
+      //     acc[attr.nodeName] = attr.nodeValue || '';
+      //     return acc;
+      //   },
+      //   {}
+      // );
+      const attributes: JsonObject = {};
+      for (let i = 0; i < xml.attributes.length; i++) {
+        const attr = xml.attributes.item(i);
+        if (attr) {
+          attributes[attr.nodeName] = attr.nodeValue || '';
+        }
+      }
+      obj['@attributes'] = attributes;
     }
   } else if (xml.nodeType === 3) {
     // 텍스트 노드
@@ -21,9 +29,24 @@ export const xmlToJson = (xml: Node): JsonObject | string => {
   }
 
   if (xml.hasChildNodes()) {
-    Array.from(xml.childNodes).forEach((item) => {
-      const nodeName: string = item.nodeName;
-      const value: JsonValue | string = xmlToJson(item);
+    // Array.from(xml.childNodes).forEach((item) => {
+    //   const nodeName: string = item.nodeName;
+    //   const value: JsonValue | string = xmlToJson(item);
+    //   if (obj[nodeName] === undefined) {
+    //     obj[nodeName] = value;
+    //   } else {
+    //     if (!Array.isArray(obj[nodeName])) {
+    //       obj[nodeName] = [obj[nodeName] as JsonValue];
+    //     }
+    //     (obj[nodeName] as JsonValue[]).push(value as JsonValue);
+    //   }
+    // });
+
+    const children = xml.childNodes;
+    for (let i = 0; i < children.length; i++) {
+      const item = children.item(i);
+      const nodeName = item.nodeName;
+      const value = xmlToJson(item);
 
       if (obj[nodeName] === undefined) {
         obj[nodeName] = value;
@@ -33,7 +56,7 @@ export const xmlToJson = (xml: Node): JsonObject | string => {
         }
         (obj[nodeName] as JsonValue[]).push(value as JsonValue);
       }
-    });
+    }
   }
 
   return obj;
